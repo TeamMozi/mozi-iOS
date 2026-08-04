b7c397d5a07aa261127c0a944bc376ec0eb9c7f1
gnoes.ios <park_juseong@naver.com>
2026-08-04 15:45:50 +0900

    feat: SocialLoginButton 및 social 아이콘 추가

---
+++ Projects/Shared/DesignSystem/Sources/Components/Social/SocialLoginProvider.swift
@@ -0,0 +1,60 @@
+import CoreGraphics
+
+public enum SocialLoginProvider: Sendable {
+    case kakao
+    case apple
+
+    public var title: String {
+        switch self {
+        case .kakao:
+            "카카오로 시작하기"
+        case .apple:
+            "Apple로 로그인"
+        }
+    }
+
+    var background: TokenColor {
+        switch self {
+        case .kakao:
+            SemanticColor.Social.kakao
+        case .apple:
+            SemanticColor.Social.appleBackground
+        }
+    }
+
+    var content: TokenColor {
+        switch self {
+        case .kakao:
+            SemanticColor.Social.kakaoContent
+        case .apple:
+            SemanticColor.Social.appleContent
+        }
+    }
+
+    var border: TokenColor? {
+        switch self {
+        case .kakao:
+            nil
+        case .apple:
+            SemanticColor.Social.appleBorder
+        }
+    }
+
+    var contentGap: CGFloat {
+        switch self {
+        case .kakao:
+            12
+        case .apple:
+            9
+        }
+    }
+
+    var iconSize: CGSize {
+        switch self {
+        case .kakao:
+            CGSize(width: 18, height: 18)
+        case .apple:
+            CGSize(width: 15, height: 18)
+        }
+    }
+}
